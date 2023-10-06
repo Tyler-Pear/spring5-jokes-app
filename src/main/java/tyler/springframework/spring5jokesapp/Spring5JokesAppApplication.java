efe971007da62403336f4b5b7e42206d181a8930
Tyler Pearson <tyler.a.pear@gmail.com>
2023-10-06 15:55:23 -0500

    Created Spring Boot project. Closes #1.

---
+++ src/main/java/tyler/springframework/spring5jokesapp/Spring5JokesAppApplication.java
@@ -0,0 +1,15 @@
+package tyler.springframework.spring5jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JokesAppApplication.class, args);
+
+        System.out.println("Hello World");
+    }
+
+}
